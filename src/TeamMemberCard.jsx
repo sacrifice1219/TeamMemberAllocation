5f94a18d08ac8d0ca2464271725002b66465d06e
Lucky Favour <luckyfavour1219@gmail.com>
2023-06-30 10:34:04 +0100

    TeamMemberAllocation

--- src/TeamMemberCard.jsx
+++ src/TeamMemberCard.jsx
@@ -4,7 +4,9 @@ import maleProfile from './images/maleProfile.jpg';
 const TeamMemberCard = ({ employee, handleEmployeeCardClick, selectedTeam }) => {
     return ( 
                    <div key={employee.id} id={employee.id} className={(employee.teamName === selectedTeam ? 'card m-2 standout' : 'card m-2')} style={{ cursor: "pointer" }} onClick={handleEmployeeCardClick}>
-                        
+
+         // eslint-disable-next-line
+        
 {   (employee.gender === 'male')?<img src={maleProfile} style={{ width: '100%', objectFit: "cover", }} className="card-img-top" /> : <img src={femaleProfile} style={{ width: '100%', objectFit: "cover", }} className="card-img-top" /> 
 }                        
                         <div className="card-body">
